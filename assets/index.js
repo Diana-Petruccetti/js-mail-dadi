d346f3340120c817aab7f6c471f1594809710aae
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-10-04 11:57:56 +0200

    exercise completed

--- assets/index.js
+++ assets/index.js
@@ -24,9 +24,11 @@ proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro program
 - Array
 */
 
+//CREA SISTEMA CONTROLLO EMAIL
 // 1. Preparazione
 // Creo variabile email
-let email
+let email;
+
 // Crea lista array di chi può accedere al gioco
 const emails = [
     'topogigio@live.it',
@@ -36,10 +38,60 @@ const emails = [
 ]
 
 // 2. Raccolta Dati
-// Chiedi all'utente la sua email e controlla che possa partecipare
+// Chiedi all'utente la sua email e controlla che possa partecipare (prompt)
 // Se può partecipare fai comparire il gioco
 // Altrimenti fai comparire messaggio adatto
+canAccess = false;
+email = prompt('Scrivi la tua email');
+
 
 // 3. Elaborazione Dati
+/* canAccess = emails.includes(email); */
+for (let i = 0; i < emails.length; i++) {
+    const thisEmail = emails [i];
 
+    if (thisEmail == email) {
+        canAccess = true;
+        break
+    }
+}
+
+console.log(canAccess, emails, email);
+
+// 4. Output
+if (canAccess) {
+    alert ('Benvenuto/a');
+} else {
+    alert ('Accesso negato');
+    
+}
+
+
+// CREA IL GIOCO
+let wannaPlayAgain = true;
+while (wannaPlayAgain) {
+// 1. Preparazione
+let playerNumber;
+let pcNumber;
+let message;
+
+// 2. Raccolta Dati
+playerNumber = Math.floor(Math.random() * 6) + 1;
+pcNumber = Math.floor(Math.random() * 6) + 1;
+
+console.log(playerNumber, pcNumber);
+
+// 3. Elaborazione Dati
+if (playerNumber > pcNumber) {
+    message = `Hai vinto! Il tuo numero è: ${playerNumber} | Il numero del PC è: ${pcNumber}`;
+} else if (playerNumber < pcNumber) {
+    message = `Hai perso! Il tuo numero è: ${playerNumber} | Il numero del PC è: ${pcNumber}`;
+} else {
+    message = `Ritenta! Il tuo numero è: ${playerNumber} | Il numero del PC è: ${pcNumber}`;
+}
+
+alert(message);
+wannaPlayAgain = confirm('Vuoi giocare ancora?');
+}
+alert ('Grazie, alla prossima!');
 // 4. Output
